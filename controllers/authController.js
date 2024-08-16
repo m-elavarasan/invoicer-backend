ec614b753f85db7d6008597091906172427ddc53
m-elavarasan <elasty2019@gmail.com>
2024-08-16 20:58:07 +0530

    Status added in auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,11 +9,11 @@ const loginUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(404).json({code:"NO_USER_FOUND", message: "User not found" });
         }
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
-            return res.status(401).json({ message: "Invalid email or password" });
+            return res.status(401).json({ code:"WRONG_PASSWORD",message: "Invalid email or password" });
         }
 
         user.lastLogin = new Date();
@@ -22,6 +22,7 @@ const loginUser = async (req, res) => {
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
         res.status(200).json({
+            code:"SUCCESS",
             token,
             user: {
                 id: user._id,
